6224101b9d72abab0bc86ddddbf0a6d889f32fb7
Ambika2024 <145181223+Ambika2024@users.noreply.github.com>
2024-06-08 18:59:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 <main>
 <h1>THE FAMOUS WRITER</h1>
 <p>Playwright and poet William Shakespeare is considered the greatest dramatist of all time. His works are loved throughout the world, but Shakespeare’s personal life is shrouded in mystery.</p>
-<img src="PHPBasicWebsite/IMAGES/th (18).jpeg" alt="WILLIAM SHAKESPEARE">
+<img src="IMAGES/th (18).jpeg" alt="SACHIN TENDULKAR">
 </main>
 <?php include 'includes/footer.php'; ?>
